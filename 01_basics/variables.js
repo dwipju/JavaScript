5b4affe70d31043192c1a75fcb0d8f0d412bcd09
dwipju <109402916+dwipju@users.noreply.github.com>
2026-02-23 12:37:01 +0000

    container config

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=4356
+let accountEmail="dwip.bauri@google.com"
+var accountPassword=5643
+accountCity="Durgapur"
+
+// accountId=7000
+accountEmail="dwip@facebook.com"
+accountPassword=9000
+accountCity="Kolkata"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
